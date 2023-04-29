b49a3025b91b253d83240a013ca7c7ba66cdb573
HinanoAira <aira@hinasense.jp>
2023-04-30 07:09:44 +0900

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const numberPower = [2, 3, 4, 5, 6, 7, 8, 9, "T", "J", "Q", "K", "A"];
+const numberPower = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "T",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
 const numbersTemplate = {
   A: 0,
   2: 0,
@@ -33,12 +47,12 @@ function IsFlush(targetCards) {
         }
       }
       for (let i = 0; i < 13; i++) {
-        if (numbers[numberPower[12 - i]] > 0) {
-          for (let j = 0; j < numbers[numberPower[12 - i]]; j++) {
-            ans += numberPower[12 - i];
-            if (ans.length === 5) {
-              break;
-            }
+        if (numbers[numberPower[12 - i]] !== 0) {
+          if (ans.length + numbers[numberPower[12 - i]] < 5) {
+            ans += numberPower[12 - i].repeat(numbers[numberPower[12 - i]]);
+          } else {
+            ans += numberPower[12 - i].repeat(5 - ans.length);
+            break;
           }
         }
       }
@@ -57,16 +71,17 @@ function IsSameNumber(targetCards, sameCount) {
       let ans = "";
       for (let i = 0; i < 13; i++) {
         if (numbers[numberPower[12 - i]] === sameCount) {
-          for (let j = 0; j < sameCount; j++) {
-            ans += numberPower[12 - i];
-          }
+          ans += numberPower[12 - i].repeat(sameCount);
+          numbers[numberPower[12 - i]] -= sameCount;
           break;
         }
       }
       for (let i = 0; i < 13; i++) {
-        if (numbers[numberPower[12 - i]] === 1) {
-          ans += numberPower[12 - i];
-          if (ans.length === 5) {
+        if (numbers[numberPower[12 - i]] !== 0) {
+          if (ans.length + numbers[numberPower[12 - i]] < 5) {
+            ans += numberPower[12 - i].repeat(numbers[numberPower[12 - i]]);
+          } else {
+            ans += numberPower[12 - i].repeat(5 - ans.length);
             break;
           }
         }
@@ -87,24 +102,19 @@ function IsTwoPairs(targetCards) {
   }
   if (sameCount[2] >= 2) {
     let ans = "";
-    let count = 0;
     for (let i = 0; i < 13; i++) {
       if (numbers[numberPower[12 - i]] === 2) {
-        for (let j = 0; j < 2; j++) {
-          ans += numberPower[12 - i];
-        }
-        count++;
-        if (count === 2) {
-          break;
-        }
+        ans += numberPower[12 - i].repeat(2);
+        numbers[numberPower[12 - i]] -= 2;
+      }
+      if (ans.length === 4) {
+        break;
       }
     }
     for (let i = 0; i < 13; i++) {
-      if (numbers[numberPower[12 - i]] === 1) {
+      if (numbers[numberPower[12 - i]] !== 0) {
         ans += numberPower[12 - i];
-        if (ans.length === 5) {
-          break;
-        }
+        break;
       }
     }
     return ans;
@@ -123,19 +133,17 @@ function IsFullHouse(targetCards) {
   if ((sameCount[3] >= 1 && sameCount[2] >= 1) || sameCount[3] >= 2) {
     let ans = "";
     for (let i = 0; i < 13; i++) {
-      if (numbers[numberPower[12 - i]] >= 2) {
-        for (let j = 0; j < (numbers[numberPower[12 - i]] === 2 ? 2 : 3); j++) {
-          ans += numberPower[12 - i];
+      if (ans.length === 0) {
+        if (numbers[numberPower[12 - i]] >= 3) {
+          ans += numberPower[12 - i].repeat(3);
+          numbers[numberPower[12 - i]] -= 3;
+          i = 0;
         }
-        for (let j = i + 1; j < 13; j++) {
-          if (numbers[numberPower[12 - j]] >= 5 - ans.length) {
-            for (let k = 0; k <= 5 - ans.length; k++) {
-              ans += numberPower[12 - j];
-            }
-            break;
-          }
+      } else {
+        if (numbers[numberPower[12 - i]] >= 2) {
+          ans += numberPower[12 - i].repeat(2);
+          break;
         }
-        break;
       }
     }
     return ans;
@@ -164,33 +172,39 @@ function IsStraight(targetCards) {
       return ans;
     }
   }
+  if (
+    numbers["A"] >= 1 &&
+    numbers[2] >= 1 &&
+    numbers[3] >= 1 &&
+    numbers[4] >= 1 &&
+    numbers[5] >= 1
+  ) {
+    return "5432A";
+  }
   return "00000";
 }
 function IsStraightFlush(targetCards) {
-  if (IsStraight(targetCards) !== "00000" && IsFlush(targetCards) !== "00000") {
-    const suits = { S: 0, H: 0, D: 0, C: 0 };
-    for (let i = 0; i < targetCards.length / 2; i++) {
-      suits[targetCards[i * 2]]++;
-    }
-    for (const key in suits) {
-      if (suits[key] >= 5) {
-        let tmp = "";
-        const numbers = Object.assign({}, numbersTemplate);
-        for (let i = 0; i < targetCards.length / 2; i++) {
-          if (targetCards[i * 2] === key) {
-            numbers[targetCards[1 + i * 2]]++;
-          }
+  const suits = { S: 0, H: 0, D: 0, C: 0 };
+  for (let i = 0; i < targetCards.length / 2; i++) {
+    suits[targetCards[i * 2]]++;
+  }
+  for (const key in suits) {
+    if (suits[key] >= 5) {
+      let ans = "";
+      const numbers = Object.assign({}, numbersTemplate);
+      for (let i = 0; i < targetCards.length / 2; i++) {
+        if (targetCards[i * 2] === key) {
+          numbers[targetCards[1 + i * 2]]++;
         }
-        for (let i = 0; i < 13; i++) {
-          if (numbers[numberPower[12 - i]] > 0) {
-            for (let j = 0; j < numbers[numberPower[12 - i]]; j++) {
-              tmp += key;
-              tmp += numberPower[12 - i];
-            }
-          }
-        }
-        return IsStraight(tmp);
       }
+      for (let i = 0; i < 13; i++) {
+        if (numbers[numberPower[12 - i]] !== 0) {
+          ans += `${key}${numberPower[12 - i]}`.repeat(
+            numbers[numberPower[12 - i]]
+          );
+        }
+      }
+      return IsStraight(ans);
     }
   }
   return "00000";
@@ -271,89 +285,94 @@ function Analyze(targetCards) {
   }
 }
 function Fight(data) {
+  const players = data.data;
   const dealer = data.dealer;
   const pods = {
     mainPod: { call: 0, pod: 0, joinedPlayer: "" },
     sidePods: [],
   };
-  for (let i = 0; i < 8; i++) {
-    const item = data.data[i];
-    if (item.power !== 0 && item.totalbet !== 0) {
-      if (pods.mainPod.call !== item.totalbet) {
-        if (pods.mainPod.call === 0) {
-          pods.mainPod.pod = item.totalbet;
-          pods.mainPod.call = item.totalbet;
-          pods.mainPod.joinedPlayer += i;
-        } else {
-          if (item.totalbet < pods.mainPod.call) {
-            const newSidePod = {
-              call: pods.mainPod.call - item.totalbet,
-              pod:
-                (pods.mainPod.call - item.totalbet) *
-                pods.mainPod.joinedPlayer.length,
-              joinedPlayer: pods.mainPod.joinedPlayer,
-            };
-            const newMainPod = {
-              call: item.totalbet,
-              pod: pods.mainPod.pod - newSidePod.pod + item.totalbet,
-              joinedPlayer: pods.mainPod.joinedPlayer + i,
-            };
-            pods.mainPod = newMainPod;
-            pods.sidePods.unshift(newSidePod);
-          } else {
-            let bet = item.totalbet;
-            pods.mainPod.pod += pods.mainPod.call;
-            pods.mainPod.joinedPlayer += i;
-            bet -= pods.mainPod.call;
-            for (let j = 0; j < pods.sidePods.length; j++) {
-              const sidePod = pods.sidePods[j];
-              if (sidePod.call <= bet) {
-                sidePod.pod += sidePod.call;
-                sidePod.joinedPlayer += i;
-                bet -= sidePod.call;
-                if (bet === 0) {
-                  break;
-                }
-              } else {
-                const newSidePod = {
-                  call: sidePod.call - bet,
-                  pod: (sidePod.call - bet) * sidePod.joinedPlayer.length,
-                  joinedPlayer: sidePod.joinedPlayer,
-                };
-                sidePod.call = bet;
-                sidePod.pod = sidePod.pod - newSidePod.pod + bet;
-                sidePod, (joinedPlayer = sidePod.joinedPlayer + i);
-                bet = 0;
-                pods.sidePods.splice(j + 1, 0, newSidePod);
-                break;
-              }
-            }
-            if (bet !== 0) {
-              const newSidePod = {
-                call: bet,
-                pod: bet,
-                joinedPlayer: `${i}`,
-              };
-              pods.sidePods.push(newSidePod);
-            }
-          }
-        }
-      } else {
+  for (let i = 0; i < players.length; i++) {
+    const item = players[i];
+    if (item.totalbet !== 0) {
+      if (item.power === 0) {
+        pods.mainPod.pod += item.totalbet;
+        continue;
+      }
+      if (pods.mainPod.call === item.totalbet) {
         pods.mainPod.pod += item.totalbet;
         pods.mainPod.joinedPlayer += i;
+        continue;
       }
-    } else if (item.totalbet !== 0) {
-      pods.mainPod.pod += item.totalbet;
+      if (pods.mainPod.call === 0) {
+        pods.mainPod.pod = item.totalbet;
+        pods.mainPod.call = item.totalbet;
+        pods.mainPod.joinedPlayer += i;
+        continue;
+      }
+      if (pods.mainPod.call > item.totalbet) {
+        pods.sidePods.unshift({
+          call: pods.mainPod.call - item.totalbet,
+          pod:
+            (pods.mainPod.call - item.totalbet) *
+            pods.mainPod.joinedPlayer.length,
+          joinedPlayer: pods.mainPod.joinedPlayer,
+        });
+        pods.mainPod.call = item.totalbet;
+        pods.mainPod.pods -= pods.sidePods[0].pod;
+        pods.mainPod.pods += item.totalbet;
+        pods.mainPod.joinedPlayer += i;
+        continue;
+      }
+      let bet = item.totalbet;
+      pods.mainPod.pod += pods.mainPod.call;
+      pods.mainPod.joinedPlayer += i;
+      bet -= pods.mainPod.call;
+      for (const [index, sidePod] of Object.entries(pods.sidePods)) {
+        if (sidePod.call <= bet) {
+          sidePod.pod += sidePod.call;
+          sidePod.joinedPlayer += i;
+          bet -= sidePod.call;
+          if (bet === 0) {
+            break;
+          }
+        } else {
+          pods.sidePods.splice(index + 1, 0, {
+            call: sidePod.call - bet,
+            pod: sidePod.call - bet * sidePod.joinedPlayer.length,
+            joinedPlayer: sidePod.joinedPlayer,
+          });
+          sidePod.call = bet;
+          sidePod.pod -= pods.sidePods[index + 1].pod;
+          sidePod.pod += bet;
+          sidePod.joinedPlayer = sidePod.joinedPlayer + i;
+          bet = 0;
+          break;
+        }
+      }
+      if (bet !== 0) {
+        pods.sidePods.push({
+          call: bet,
+          pod: bet,
+          joinedPlayer: `${i}`,
+        });
+      }
     }
   }
-  const players = data.data;
-  const result = [0, 0, 0, 0, 0, 0, 0, 0];
-  const resultSidePod = ["", "", "", "", "", "", "", ""];
-  let sidePodLevel = 1;
-  while (pods.mainPod.pod !== 0 || pods.sidePods.length !== 0) {
+  if (
+    pods.sidePods.length > 0 &&
+    pods.sidePods[pods.sidePods.length - 1].joinedPlayer.length == 1
+  ) {
+    pods.sidePods.pop();
+  }
+  const result = JSON.parse(
+    `[${'{"prize":0,"text":""},'.repeat(players.length).slice(0, -1)}]`
+  );
+  const mainPod = JSON.parse(JSON.stringify(pods.mainPod));
+  const sidePods = JSON.parse(JSON.stringify(pods.sidePods));
+  while (mainPod.pod !== 0 || sidePods.length !== 0) {
     let topPower = 0;
     let topPlayers = "";
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < players.length; i++) {
       const item = players[i];
       if (item.power > topPower) {
         topPower = item.power;
@@ -362,103 +381,50 @@ function Fight(data) {
         topPlayers += i;
       }
     }
-    for (let i = 0; i < topPlayers.length; i++) {
-      players[topPlayers[i]].power = 0;
-    }
-    if (topPlayers.length === 1) {
-      if (pods.mainPod.pod !== 0) {
-        result[topPlayers] += pods.mainPod.pod;
-        resultSidePod[
-          topPlayers
-        ] = `MainPod:${pods.mainPod.pod} > ${pods.mainPod.pod}`;
-        pods.mainPod.pod = 0;
-      }
-      while (pods.sidePods.length !== 0) {
-        if (pods.sidePods[0].joinedPlayer.includes(topPlayers)) {
-          result[topPlayers] += pods.sidePods[0].pod;
-          if (resultSidePod[topPlayers] !== "") {
-            resultSidePod[topPlayers] += "\n";
-          }
-          resultSidePod[
-            topPlayers
-          ] += `SidePod${sidePodLevel}:${pods.sidePods[0].pod} > ${pods.sidePods[0].pod}`;
-          sidePodLevel++;
-          pods.sidePods.shift();
-        } else {
-          break;
+    for (
+      let i = dealer;
+      topPlayers.length !== 0;
+      i = (i + 1) % players.length
+    ) {
+      if (players[i].power === topPower) {
+        if (mainPod.pod !== 0) {
+          const prize = Math.ceil(mainPod.pod / topPlayers.length);
+          result[i].prize += prize;
+          result[i].text += `\nMainPod ${pods.mainPod.pod} > ${prize}`;
+          mainPod.pod -= prize;
         }
-      }
-    } else {
-      let tmp = topPlayers;
-      let tmpPod = pods.mainPod.pod;
-      while (pods.mainPod.pod !== 0) {
-        for (let i = 0; i < tmp.length; i++) {
-          result[tmp[i]] += Math.floor(pods.mainPod.pod / tmp.length);
-        }
-        pods.mainPod.pod -=
-          Math.floor(pods.mainPod.pod / tmp.length) * tmp.length;
-        for (let i = dealer; true; i--) {
-          if (i === -1) {
-            i = 7;
-          }
-          if (tmp.includes(i)) {
-            tmp = tmp.replace(`${i}`, "");
-            break;
-          }
-        }
-      }
-      if (tmpPod !== 0) {
-        for (let i = 0; i < topPlayers.length; i++) {
-          resultSidePod[topPlayers[i]] = `MainPod:${tmpPod} > ${
-            result[topPlayers[i]]
-          }`;
-        }
-      }
-      tmp = topPlayers;
-      while (pods.sidePods.length !== 0) {
-        tmpPod = pods.sidePods[0].pod;
-        const tmpResult = Object.assign({}, result);
-        while (pods.sidePods[0].pod !== 0) {
-          const tmp2 = tmp;
-          for (let i = 0; i < tmp2.length; i++) {
-            if (!pods.sidePods[0].joinedPlayer.includes(tmp2[i])) {
-              tmp = tmp.replace(tmp2[i], "");
+        for (const [index, sidePod] of Object.entries(sidePods)) {
+          if (sidePod.joinedPlayer.includes(i)) {
+            let count = 0;
+            for (const player of topPlayers) {
+              if (sidePod.joinedPlayer.includes(player)) {
+                count++;
+              }
             }
-          }
-          for (let i = 0; i < tmp.length; i++) {
-            result[tmp[i]] += Math.floor(pods.sidePods[0].pod / tmp.length);
-          }
-          pods.sidePods[0].pod -=
-            Math.floor(pods.sidePods[0].pod / tmp.length) * tmp.length;
-          for (let i = dealer; true; i--) {
-            if (i === -1) {
-              i = 7;
-            }
-            if (tmp.includes(i)) {
-              tmp = tmp.replace(i, "");
-              break;
-            }
+            const prize = Math.ceil(sidePod.pod / count);
+            result[i].prize += prize;
+            result[
+              i
+            ].text += `\nSidePod${index} ${pods.sidePods[index].pod} > ${prize}`;
+            sidePod.pod -= prize;
           }
         }
-        for (let i = 0; i < 8; i++) {
-          if (result[i] !== tmpResult[i]) {
-            if (resultSidePod[i] !== "") {
-              resultSidePod[i] += "\n";
-            }
-            resultSidePod[i] += `SidePod${sidePodLevel}:${tmpPod} > ${
-              result[i] - tmpResult[i]
-            }`;
-          }
-        }
-        sidePodLevel++;
-        pods.sidePods.shift();
-        if (tmp === "") {
-          break;
+        players[i].power = 0;
+        topPlayers = topPlayers.replace(i, "");
+      }
+      for (let j = sidePods.length - 1; j >= 0; j--) {
+        if (sidePods[j].pod === 0) {
+          sidePods.splice(j, 1);
+          pods.sidePods.splice(j, 1);
         }
       }
     }
   }
-  return `${result[0]} WIN!\n${resultSidePod[0]},${result[1]} WIN!\n${resultSidePod[1]},${result[2]} WIN!\n${resultSidePod[2]},${result[3]} WIN!\n${resultSidePod[3]},${result[4]} WIN!\n${resultSidePod[4]},${result[5]} WIN!\n${resultSidePod[5]},${result[6]} WIN!\n${resultSidePod[6]},${result[7]} WIN!\n${resultSidePod[7]},`;
+  let ans = "";
+  for (let i = 0; i < players.length; i++) {
+    ans += `${result[i].prize} WIN!${result[i].text},`;
+  }
+  return ans.slice(0, -1);
 }
 
 app.use(
